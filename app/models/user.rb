71153b7b042d207731d640e0b2691f71aafaab21
Colby Cox <coxjc@seas.upenn.edu>
2016-12-26 17:24:56 -0500

    SUBJ: User model MSG: 1) Add more validators to User model, such as password length 2) Add method to save name and email as uppercase upon saving of User 3) Add tests for these methods and further implementations 4) Add basic route for User 5) Generate scaffold to create views and controller for User.. no work done on these.. 6) Update .gitignore for IntelliJ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ApplicationRecord
 
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-    validates :name, :email, :presence => true
-    validates :email, :uniqueness => true
-    validates :email, format: {with: VALID_EMAIL_REGEX}
+  validates :name, :email, :password, :password_confirmation, :presence => true
+  validates :email, :uniqueness => true
+  validates :email, format: {with: VALID_EMAIL_REGEX}
+  validates :password, :length => {minimum: 6}
 
-    has_secure_password
-    
+  before_save :save_uppercase
+
+  def save_uppercase
+    self.name.upcase!
+    self.email.upcase!
+  end
+
+  has_secure_password
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,49 +1,74 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
+  # test 'the truth' do
   #   assert true
   # end
-  def setup 
-      @user = User.new(:name => "TestingName", :email => "name@domain.net", :password => "password", :password_confirmation => "password")
-      @user_2 = User.new(:name => "TestingName2", :email => "name@domain.com", :password => "password", :password_confirmation => "password")
+  def setup
+    @user = User.new(:name => 'TestingName', :email => 'name@domain.net', :password => 'password', :password_confirmation => 'password')
+    @user_2 = User.new(:name => 'TestingName2', :email => 'name@domain.com', :password => 'password', :password_confirmation => 'password')
   end
-  
-  test "testing test suite" do
+
+  test 'testing test suite' do
       assert true
       assert_not false
-  end 
+  end
 
-  test "valid user" do 
+  test 'valid user' do
       assert @user.valid? 
   end
 
-  test "invalid without name" do 
+  test 'invalid without name' do
       @user.name = nil
       assert_not @user.valid? 
   end
 
-  test "invalid without email" do
+  test 'invalid without email' do
       @user.email = nil
       assert_not @user.valid?
    end
 
-  test "invalid without password & password confirmation" do
+  test 'invalid without password & password confirmation' do
       @user.password = nil
       @user.password_confirmation = nil
       assert_not @user.valid?
   end
 
-  test "invalid with mismatched password & password confirmation" do
-      @user.password = "wrongpassword"
-      @user.password_confirmation = "password"
+  test 'invalid with mismatched password & password confirmation' do
+    @user.password = 'wrongpassword'
+    @user.password_confirmation = 'password'
       assert_not @user.valid?
-  end 
+  end
 
-  test "invalid without unique email" do
+  test 'invalid w/ password less than 6 characters long' do
+    @user.password = @user.password_confirmation = 'passw'
+    assert_not @user.valid?
+    @user.password = @user.password_confirmation = 'password'
+    assert @user.valid?
+  end
+
+  test 'invalid without unique email' do
       @user_2.email = @user.email
       @user.save!
       assert_not @user_2.valid?
+  end
+
+  test 'name saves as uppercase' do
+    user_name = @user.name.upcase
+    @user.save!
+    assert @user.name == user_name
+    user_2_name = @user_2.name.upcase
+    @user_2.save!
+    assert @user_2.name == user_2_name
+  end
+
+  test 'email saves as uppercase' do
+    user_email = @user.email.upcase
+    @user.save!
+    assert @user.email == user_email
+    user_2_email = @user_2.email.upcase
+    @user_2.save!
+    assert @user_2.email = user_2_email
   end
 
 end
