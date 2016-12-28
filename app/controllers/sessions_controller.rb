d7380f48b47fccd61f823488bc7e3371ba96c5a5
Colby Cox <coxjc@seas.upenn.edu>
2016-12-27 21:02:15 -0500

    SUBJ: Sessions, navbar MSG: 1) Add Bootstrap navbar 2) Add routes for login and logout 3) Comment out controller tests for users... need work here 4) Added testing 5) Created login page and other things WIP: Logout, navbar styling

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].upcase)
+    if user && user.authenticate(params[:session][:password])
+      login user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid credentials'
+      render 'new'
+    end
+  end
+
+end
