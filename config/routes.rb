31cde9af56d9c063a7456a6c0649b60987b22c75
Colby Cox <coxjc@seas.upenn.edu>
2017-01-13 18:09:16 -0500

    SUBJ: Home page, root, etc. MSG: 1) Update navbar home link 2) Create root route w/ home static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  root :to => 'users#index'
+  root :to => 'pages#show', page: 'home'
+
+  get "/pages/:page" => "pages#show"
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   resources :keystores
   resources :users
-  get "/pages/:page" => "pages#show"
+
 end
