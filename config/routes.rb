d2c8017512d373f41578fa3c4566c24283b6bd71
Colby Cox <coxjc@seas.upenn.edu>
2017-01-13 18:03:11 -0500

    SUBJ: Static Pages MSG: 1) Create Pages controller for static pages 2) Create template home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :keystores
   root :to => 'users#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :keystores
   resources :users
+  get "/pages/:page" => "pages#show"
 end
